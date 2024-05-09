54278dd2eb1e882fdddc9dacce588b920e207826
volemy <minayobenevolence0@gamil.com>
2024-05-09 15:48:48 +0300

    task 1/2

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,8 @@
+-- sql script that ranks origins of bands
+-- Ordered by the number of (non-unique) fans
+-- Column names must be: origin and nb_fans
+
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
