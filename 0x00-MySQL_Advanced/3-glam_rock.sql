7a780762dc7f2f931c234ffd4793c5a3b82f7d6b
volemy <minayobenevolence0@gamil.com>
2024-05-09 16:04:26 +0300

    task 3/u

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,8 +2,7 @@
 -- ranked by their longevity
 -- Column names must be: band_name & lifespan
 
-
-SELECT band_name, ifnull(split, 2020)-ifnull(formed, 0) AS lifespan
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC
